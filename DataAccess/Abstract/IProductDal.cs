b184a92c6849fe10dc0c5e7a36671fcd5313cd5c
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-03-04 01:00:40 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //interface metotlar publictir ancak interface in kendisi public degildir
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        
+    }
+}
